f5314f04d1d7af13e2ebd569a74f9c1969279e6a
Lucas Caussat <lcaussat@IUT.BX1>
2019-02-01 11:48:17 +0100

    style deplacement + position

--- 16.5.Examiner.cxx
+++ 16.5.Examiner.cxx
@@ -44,59 +44,145 @@
 #include <Inventor/nodes/SoTranslation.h>
 #include <Inventor/nodes/SoScale.h>
 #include <Inventor/nodes/SoTexture2.h>
+#include <Inventor/engines/SoCalculator.h>
+#include <Inventor/engines/SoTimeCounter.h>
+#include <Inventor/nodes/SoFont.h>
+#include <Inventor/nodes/SoText3.h>
+#include <Inventor/sensors/SoFieldSensor.h>
+#include <Inventor/fields/SoField.h>
+
+SoTranslation *mainTranslation;
+SoText3 *myText;
+
+static void setText(void *, SoSensor *s)
+{
+	printf("Animation step\n");
+
+	SbVec3f vecteur = mainTranslation->translation.getValue();
+
+	char tmpStr[256];
+	sprintf(tmpStr, "%f, %f", vecteur[0], vecteur[2]);
+	myText->string = tmpStr;
+	
+
+}
 
 int
 main(int, char **argv)
 {
   // Initialize Inventor and Xt
   Widget myWindow = SoXt::init(argv[0]);
-  
   // Build the viewer in the applications main window
   SoXtExaminerViewer *myViewer = 
     new SoXtExaminerViewer(myWindow);
 
+
   SoSeparator *pRoot = new SoSeparator();
+
   pRoot->ref();
+
+
+  mainTranslation = new SoTranslation;
+  pRoot->addChild(mainTranslation);
+  // Set up an engine to calculate the motion path:
+  // r = 5*cos(5*theta); x = r*cos(theta); z = r*sin(theta)
+  // Theta is incremented using a time counter engine,
+  // and converted to radians using an expression in
+  // the calculator engine.
+  SoCalculator *calcXZ = new SoCalculator;
+  SoTimeCounter *thetaCounter = new SoTimeCounter;
+  thetaCounter->max = 360;
+  thetaCounter->step = 4;
+  thetaCounter->frequency = 0.075;
+  calcXZ->a.connectFrom(&thetaCounter->output);
+  calcXZ->expression.set1Value(0, "ta=a*M_PI/180"); // theta
+  calcXZ->expression.set1Value(1, "tb=5*cos(5*ta)"); // r
+  calcXZ->expression.set1Value(2, "td=tb*cos(ta)"); // x
+  calcXZ->expression.set1Value(3, "te=tb*sin(ta)"); // z
+  calcXZ->expression.set1Value(4, "oA=vec3f(td,0,te)");
+  mainTranslation->translation.connectFrom(&calcXZ->oA);
+
+
+  SoFieldSensor *mySensor = new SoFieldSensor;
+  mySensor->setFunction(setText);
+  mySensor->attach(&mainTranslation->translation);
+
+
+  SoSeparator *pText = new SoSeparator();
+  pRoot->addChild(pText);
+
+  SoSeparator *pParts = new SoSeparator();
+  pRoot->addChild(pParts);
+
+  SoTranslation *textTranslate = new SoTranslation;
+  textTranslate->translation.setValue(0.0, -13.0, 0.0);
+  pText->addChild(textTranslate);
+
+  //TEXTE
+
+  SoFont *myFont = new SoFont;
+  myFont->name.setValue("Times-Roman");
+  myFont->size.setValue(2.0);
+  myText = new SoText3;
+  myText->spacing = .5;
+  myText->justification = SoText3::CENTER;
+  myText->parts = SoText3::ALL;
+
+  pText->addChild(myFont);
+  pText->addChild(myText);
+
+
+
+  //tête
 
   SoCone *pCone = new SoCone();
 
   SoTexture2  *tBois = new SoTexture2();
   tBois->filename = "C:\\Open Inventor\\9.9.0 C++ Visual2017 x64\\textures\\bois.jpg";
-  pRoot->addChild(tBois);
+  pParts->addChild(tBois);
 
-  pRoot->addChild(pCone);
+  pParts->addChild(pCone);
+  
+
+
+  //Corp
 
   SoCylinder *pCylinder = new SoCylinder();
   pCylinder->height.setValue(10);
 
   SoTranslation *leftTranslation = new SoTranslation;
   leftTranslation->translation.setValue(0.0, -6.0, 0.0);
-  pRoot->addChild(leftTranslation);
+  pParts->addChild(leftTranslation);
 
 
   SoTexture2  *tRouge = new SoTexture2();
   tRouge->filename = "C:\\Open Inventor\\9.9.0 C++ Visual2017 x64\\textures\\rouge.jpg";
-  pRoot->addChild(tRouge);
+  pParts->addChild(tRouge);
 
-  pRoot->addChild(pCylinder);
+  pParts->addChild(pCylinder);
+
+  //Mine
+
 
   SoCylinder *pCylinder2 = new SoCylinder();
 
   SoTranslation *leftTranslation2 = new SoTranslation;
   leftTranslation2->translation.setValue(0.0, 6.8, 0.0);
-  pRoot->addChild(leftTranslation2);
+  pParts->addChild(leftTranslation2);
 
   SoScale *pScale = new SoScale();
   pScale->scaleFactor.setValue(0.1, 0.3, 0.1);
   
-  pRoot->addChild(pScale);
+  pParts->addChild(pScale);
+
 
   SoTexture2  *tGraphite = new SoTexture2();
   tGraphite->filename = "C:\\Open Inventor\\9.9.0 C++ Visual2017 x64\\textures\\graphite.jpg";
-  pRoot->addChild(tGraphite);
+  pParts->addChild(tGraphite);
 
-  pRoot->addChild(pCylinder2);
+  pParts->addChild(pCylinder2);
   
+
   // Attach the viewer to the scene graph
   myViewer->setSceneGraph(pRoot);
   
@@ -111,5 +197,3 @@ main(int, char **argv)
   
   return 0;
 }
-
-
